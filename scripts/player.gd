155c579ee98598695b9bb5d616d51e33b2737e96
Juan Ocaña <juanov98@gmail.com>
2019-10-15 11:20:55 +0200

    Rename project

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-const grav = 10
+const grav = 5
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,7 +13,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	self.position.y += grav
+	self.position.y -= grav
 	
 	if $AnimationPlayer.current_animation != "walk":
 		$AnimationPlayer.play("walk")
